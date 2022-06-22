c6f50ad167f4da97d5b7921b725bc178ff95bb13
Adrien Pauchet <grandchefdotkong@gmail.com>
2022-06-22 17:23:45 +0900

    first commit

---
+++ src/context/ShoppingCartContext.tsx
@@ -0,0 +1,110 @@
+import { createContext, ReactNode, useContext, useState } from "react";
+import ShoppingCart from "../components/ShoppingCart";
+import { useLocalStorage } from '../hooks/useLocalStorage';
+
+type ShoppingCartProviderProps = {
+    children: ReactNode
+}
+
+type CartItem = {
+    id: number,
+    quantity: number
+}
+
+type ShoppingCartContext = {
+    openCart: () => void,
+    closeCart: () => void,
+    cartQuantity: number,
+    cartItems: CartItem[],
+    getItemQuantity: (id: number) => number,
+    increaseCartQuantity: (id: number) => void,
+    decreaseCartQuantity: (id: number) => void,
+    removeFromCart: (id: number) => void,
+
+}
+
+const ShoppingCartContext = createContext({} as 
+  ShoppingCartContext);
+
+export function useShoppingCart() {
+  return useContext(ShoppingCartContext)
+};
+
+export function ShoppingCartProvider(
+{ children}: ShoppingCartProviderProps) {
+  const [cartItems, setCartItems] = useLocalStorage<CartItem[]>("shopping-cart", []);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const cartQuantity = 
+    cartItems.reduce((quantity, item) => item.quantity + quantity, 0)
+
+  function getItemQuantity(id: number) {
+    return cartItems.find(item => item.id === id)?.quantity || 0;
+  }
+
+  function increaseCartQuantity(id: number) {
+    setCartItems(currItems => {
+        if(currItems.find(item => item.id === id) == null) {
+          return [...currItems, { id, quantity: 1 }]
+        }
+
+        return currItems.map(item => {
+          if(item.id === id) {
+            return { ...item, quantity: item.quantity++ }
+          }
+
+          return item;
+        });
+
+    });  
+  }
+
+  function removeFromCart(id: number) {
+    setCartItems(currItems => {
+      return currItems.filter(item => item.id !== id);
+    });  
+  }
+
+  function decreaseCartQuantity(id: number) {
+
+    setCartItems(currItems => {
+        if(!currItems.find(item => item.id === 1)) {
+          return currItems.filter(item => item.id !== id);
+        }
+
+        return currItems.map(item => {
+          if(item.id === id) {
+            return { ...item, quantity: item.quantity-- }
+          }
+
+          return item;
+        })
+
+    })    
+  }
+
+  const openCart = () => setIsOpen(true);
+
+  const closeCart = () => setIsOpen(false);
+
+  return <ShoppingCartContext.Provider 
+    value={{ getItemQuantity, increaseCartQuantity, decreaseCartQuantity, 
+      openCart, closeCart, removeFromCart, cartItems, 
+      cartQuantity }}
+  >
+    { children }
+    <ShoppingCart isOpen={isOpen} />
+  </ShoppingCartContext.Provider>
+}
+
+
+
+
+
+
+
+
+
+
+
+
